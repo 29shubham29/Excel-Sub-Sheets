11543b4ef42620fadefc7bf9b3879eea2c3eea8c
Shubham Pandey <shubham@spacemonk.io>
2020-09-29 19:51:10 +0530

    Adds getting subsheets from a excel file to json.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import XLSX, { read } from 'xlsx';
 
 function App() {
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+
+    let files = e.target.files,
+    file = files[0];
+    let reader = new FileReader();
+    reader.onload = function (evnt) {
+        var data = evnt.target.result;
+        let readedData = XLSX.read(data, {type: 'binary'});
+        readedData.SheetNames.map((fileName) => {
+          console.log(fileName)
+          const row = XLSX.utils.sheet_to_json(readedData.Sheets[fileName])
+          // use sheet_to_csv for converting in csv
+          // use sheet_to_html for conversion in html
+        })
+    };
+    reader.readAsBinaryString(file)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <input 
+    type='file'
+    accept='.xlsx'
+    onChange={handleUpload}/>
   );
 }
 
